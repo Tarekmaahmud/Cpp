cdefff081de8fe24ebefe2f8b18a38b6ef670cb5
Tarek Mahmud <shantopixel7pro@gmail.com>
2025-06-02 15:46:54 +0600

    marge

---
+++ algorithom/marge_sort.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+// Merge two sorted subarrays: arr[left..mid] and arr[mid+1..right]
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1; // size of left half
+    int n2 = right - mid;    // size of right half
+
+    // Create temp arrays
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    // Merge the two halves
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    // Copy remaining elements (if any)
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+// Recursive Merge Sort
+void mergeSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+// Print the array
+void printArray(int arr[], int size);
+
+// Main function
+int main()
+{
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    mergeSort(arr, 0, size - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr, size);
+
+    return 0;
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
